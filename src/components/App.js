48487a4137e0d93c3d8df05e55b6615224b8dabb
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-09-12 17:09:20 -0700

    Builds logic to map over randomized list of students

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,19 @@
+const { randomizer } = require('./randomizer')
+const { useState } = require('react');
+const Pair = require('./Pair')
+
 function App() {
+
+  const [listOfStudents, setListOfStudents] = useState(['Bob', 'Charles', 'Jeff', 'Mag', 'Rob', 'Michael', 'George'])
+console.log(randomizer.randomize(listOfStudents))
   return (
-    <div className="App">
-     
-    </div>
+        <div>
+          {randomizer.randomize(listOfStudents).map((elem) => {
+            return (
+              <Pair elem={elem} />
+             )
+          })}
+        </div>
   );
 }
 
