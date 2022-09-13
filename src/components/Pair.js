09a49d61f42d688bbab8a38f6de77cded1903f60
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-09-12 17:09:07 -0700

    Builds basic pair component

---
+++ src/components/Pair.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Pair (props) {
+    return (
+        <>
+            <li>{props[0]}</li>
+            <li>{props[1]}</li>
+        </>
+    )
+}
