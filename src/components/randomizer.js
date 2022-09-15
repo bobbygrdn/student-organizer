720caac1980229fdee9bc851d8dca2e09a709a4a
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-09-15 07:46:20 -0700

    Finishes the logic for the randomizer function

--- src/components/randomizer.js
+++ src/components/randomizer.js
@@ -1,31 +1,30 @@
 let randomizer = {
 
-    randomize: function(listOfStudents) {
-        let copy = [...listOfStudents];
+    randomize: function(studentsArray, groupSize) {
+        let copy = [...studentsArray];
         let result = []
+        let newSize = groupSize+1
 
         while(copy.length !== 0) {
-            if(copy.length === 3) {
-                let p1 = copy.splice(Math.floor(Math.random() * copy.length), 1).toString()
-                let p2 = copy.splice(Math.floor(Math.random() * copy.length), 1).toString()
-                let p3 = copy.splice(Math.floor(Math.random() * copy.length), 1).toString()
-                let newObj = {
-                    P1: p1,
-                    P2: p2,
-                    P3: p3
+            if(copy.length < groupSize) {
+                for(let i = 0; i <= copy.length+1; i++) {
+                    if(i === result.length) {
+                        i = 0;
+                        newSize++
+                    }
+                    let elem = copy.splice(Math.floor(Math.random() * copy.length), 1).toString();
+                    if(elem !== '') {
+                        result[i][`P${newSize}`] = elem
+                    }
                 }
-                result.push(newObj)
             } else {
-                let p1 = copy.splice(Math.floor(Math.random() * copy.length), 1).toString()
-                let p2 = copy.splice(Math.floor(Math.random() * copy.length), 1).toString()
-                let newObj = {
-                    P1: p1,
-                    P2: p2
+                let newObj = {};
+                for(let i = 1; i <= groupSize; i++) {
+                    let elem = copy.splice(Math.floor(Math.random() * copy.length), 1).toString();
+                    newObj[`P${i}`] = elem
                 }
-
                 result.push(newObj)
             }
-
         }
         return result;
     }
