eb4dd7c9293ea5de54d38b98ef20b8e967bd6d3a
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-09-15 07:46:08 -0700

    Updates the logic to dynamically render each list item

--- src/components/Pair.js
+++ src/components/Pair.js
@@ -2,22 +2,14 @@ import React from 'react'
 
 export default function Pair (elem) {
 
-    if(elem.elem.P3 !== undefined) {
-        return (
-            <div>
-            <h3>Pair: {elem.id+1}</h3>
-            <li>{elem.elem.P1}</li>
-            <li>{elem.elem.P2}</li>
-            <li>{elem.elem.P3}</li>
-            </div>
-        )
-    } else {
-        return (
-            <div>
-            <h3>Pair: {elem.id+1}</h3>
-            <li>{elem.elem.P1}</li>
-            <li>{elem.elem.P2}</li>
-            </div>
-        )
-    }
+    return (
+        <div>
+        <h3>Pair: {elem.id+1}</h3>
+        {Object.values(elem.elem).map((elem) => {
+            return (
+            <li>{elem}</li>
+            )
+        })}
+        </div>
+    )
 }
