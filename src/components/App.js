c4eab1f1d96fa26c3fdf48acc651aa182cadc193
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-09-15 07:45:54 -0700

    Updates the group size to test rendering

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,43 @@ const { useState } = require('react');
 
 function App() {
 
-  const [listOfStudents, setListOfStudents] = useState(['Bob', 'Charles', 'Jeff', 'Mag', 'Rob', 'Michael', 'George'])
+  const [listOfStudents, setListOfStudents] = useState(['Adam Jones',
+  'Dre Hurtado',
+  'Anthony England',
+  'Idris Yusuf',
+  'Bolun Yang',
+  'Brandon Adviento',
+  'Chris Stevens',
+  'Derek Tran',
+  'Dylan Clark',
+  'Edgar Garcia',
+  'Elijah Stamp',
+  'Ethan Charles',
+  'Gerardo Reyes',
+  'Glenn Banks',
+  'Hali Usman-Isiaka',
+  'Jakari Thomas',
+  'Jeff Kearns',
+  'Jeff Kelley',
+  'Joe Low',
+  'Joshua Richmond',
+  'Katie Chevez',
+  'Luis Guzman',
+  'Marc Menard',
+  'Nate Morrison',
+  'Noah Gragg',
+  'Rene Marquez',
+  'Ryan Barnett',
+  'Shelton Seaborn',
+  'Steven Segerlund',
+  'Tim Lott'])
+  const [groupSize, setGroupSize] = useState(6)
 
-  const random = randomizer.randomize(listOfStudents)
+  const random = randomizer.randomize(listOfStudents, groupSize)
 
   return (
         <ul>
-          {random.map((elem, index) => {       
+        {random.map((elem, index) => {       
             return (
               <Pair elem={elem} key={index} id={index} />
              )
